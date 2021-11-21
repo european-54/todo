87d32f528d51bf5cd404ae5ee7cd1e99b78289a7
european <artik-54@mail.ru>
2021-11-21 12:34:27 +0300

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Users(models.Model):
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=128)
+
+# Create your models here.
