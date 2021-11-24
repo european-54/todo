db3c43e081e4cd9810949fb2ed29a8b40e769b82
european <artik-54@mail.ru>
2021-11-25 01:45:13 +0300

    25.11.21

--- todo/settings.py
+++ todo/settings.py
@@ -16,10 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production
 SECRET_KEY = 'django-insecure-b%2afn3m+r4*$m!a1rkfl1ucb^uaz888bbdz30k_159wo$+3)_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
