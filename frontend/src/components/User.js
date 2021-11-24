c72952f5c2c973fa5f24929a01ae4be507e614b9
european <artik-54@mail.ru>
2021-11-25 02:39:25 +0300

    25.11.21

---
+++ frontend/src/components/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.birthday_year}
+            </td>
+            </tr>
+    )
+}
