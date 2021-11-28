ad50f5d522983a9c17870f2dcc18c66421c4eb33
european <artik-54@mail.ru>
2021-11-28 04:14:43 +0300

    26.11.21

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,47 +1,22 @@
 import React from 'react'
 
-
 const UserItem = ({user}) => {
-    user.first_name = undefined;
     user.last_name = undefined;
+    user.first_name = undefined;
     return (
-       <tr>
-           <td>
-               {user.username}
-           </td>
-           <td>
-               {user.first_name}
-           </td>
-           <td>
-               {user.last_name}
-           </td>
-           <td>
-               {user.email}
-           </td>
-       </tr>
-   )
+        <tr>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
 }
-
-
-const UserList = ({users}) => {
-   return (
-       <table>
-           <th>
-               Username
-           </th>
-           <th>
-               Firs_name
-           </th>
-           <th>
-               Last_name
-           </th>
-           <th>
-               Email
-           </th>
-           {users.map((user) => <UserItem user={user} />)}
-       </table>
-   )
-}
-
-
-export default UserList
