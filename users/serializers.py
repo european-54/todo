04edaa8185093629598674a7ff1233242b52f78a
european <artik-54@mail.ru>
2021-11-30 00:04:55 +0300

    28.11.21

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,11 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+
 from .models import Users
 
 
-class UserModelSerializer(HyperlinkedModelSerializer):
+class UserModelSerializer(ModelSerializer):
+
     class Meta:
         model = Users
         fields = '__all__'
