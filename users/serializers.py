34bcc03a7130101ffd1e3344b4d6a03a7a776577
european <artik-54@mail.ru>
2021-12-04 06:08:32 +0300

    4.12.21

--- users/serializers.py
+++ users/serializers.py
@@ -9,3 +9,22 @@ class UserModelSerializer(ModelSerializer):
     class Meta:
         model = Users
         fields = '__all__'
+
+
+class GuestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Guest
+        fields = ("name", "age", "phone", "email")
+
+
+class NotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ("todo_1", "todo_2", "todo_3")
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ("name", "location", "phone", "email")
+
