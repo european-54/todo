18abb448249304d983cf2a5a9e7504cbd4256d17
european <artik-54@mail.ru>
2021-12-04 04:19:05 +0300

    4.12.21

--- users/models.py
+++ users/models.py
@@ -7,3 +7,10 @@ class Users(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     email = models.EmailField(max_length=128)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.name = None
+
+    def __str__(self) -> str:
+        return self.name
