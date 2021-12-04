62c5519d92cb922d630973d0b8dd94a94b7c97d3
european <artik-54@mail.ru>
2021-12-04 06:00:03 +0300

    4.12.21

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
 
-from .models import Users
+from .models import Users, Guest, Notes, Project
 
 
 class UserModelSerializer(ModelSerializer):
