d618bbd54671ba68b3afc093a9f74543151906e3
european <artik-54@mail.ru>
2021-12-04 05:05:02 +0300

    4.12.21

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,4 @@ from django.contrib import admin
 from .models import Guest, Notes, Users, Project
 admin.site.register(Users)
 admin.site.register(Guest)
-admin.site.register(Notes)
-admin.site.register(Project)
 
