8e56fef11e8d9abfdab13a3341485e54e21d849c
european <artik-54@mail.ru>
2021-12-07 07:22:06 +0300

    7.12.21

--- users/models.py
+++ users/models.py
@@ -7,4 +7,13 @@ class Users(models.Model):
     last_name = models.CharField(max_length=64)
     email = models.EmailField(max_length=128)
 
-# Create your models here.
+
+class Guest(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField(default=20)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
